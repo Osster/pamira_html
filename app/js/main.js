aed7573027d57e97e39a6db24ca5d5be30b35b1c
Osster <_oster_@mail.ru>
2018-05-25 02:52:12 +0300

    Initial Commit

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+// Импортируем jQuery
+//= scripts/jquery-3.3.1.min.js
+
+// Импортируем Popper
+//= scripts/popper.js
+
+// Импортируем Bootstrap
+//= scripts/bootstrap.js
+
+
+//= ../../node_modules/jquery-mask-plugin/dist/jquery.mask.js
+//= ../../node_modules/jquery-validation/dist/jquery.validate.js
+//= ../../node_modules/jquery-datetimepicker/build/jquery.datetimepicker.full.min.js
+
+$(document).ready(function () {
+
+});
+
+$(window).load(function () {
+
+});
